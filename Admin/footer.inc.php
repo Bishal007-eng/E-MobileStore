007358fb99cf34847b668019144e692b0c9b1d97
Bishal Thapa <bishal.thap3@gmail.com>
2022-10-29 22:43:21 +0545

    Admin dashboard

--- Admin/footer.inc.php
+++ Admin/footer.inc.php
@@ -3,13 +3,17 @@
         <div class="footer-inner bg-white">
           <div class="row">
             <div class="col-sm-6">
-              Copyright &copy; <?php echo date('Y')?> <a href="https://www.instagram.com/bishal_thapa007/">bishal_thapa007 :D</a>
+              Copyright &copy; 2018 <a href="https://www.instagram.com/bishal_thapa007/">Bishal Thapa</a>
             </div>
-                  
+            <div class="col-sm-6 text-right">
+              Designed by <a href="https://www.instagram.com/bishal_thapa007/">Bishal Thapa</a>
+            </div>
           </div>
         </div>
       </footer>
     </div>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <script src="assets/js/vendor/jquery-2.1.4.min.js" type="text/javascript"></script>
     <script src="assets/js/popper.min.js" type="text/javascript"></script>
     <script src="assets/js/plugins.js" type="text/javascript"></script>
