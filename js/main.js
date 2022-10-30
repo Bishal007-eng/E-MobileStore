74f4f54ef947e37a12f66606c9ef13d0265d827a
Bishal Thapa <bishal.thap3@gmail.com>
2022-10-30 22:26:50 +0545

    Ecommerce

--- js/main.js
+++ js/main.js
@@ -430,3 +430,44 @@
 
 
 })(jQuery);
+
+
+function send_message()
+{
+    var name = jQuery("#name").val();
+    var email = jQuery("#email").val();
+    var mobile = jQuery("#mobile").val();
+    var message = jQuery("#message").val();
+
+    if(name=="")
+    {
+        alert('Please Enter Name');
+    }
+    else if(email=="")
+    {
+        alert('Please Enter Email');
+    }
+    else if(mobile=="")
+    {
+        alert('Please Enter Mobile number');
+    }
+    else if(message=="")
+    {
+        alert('Please Enter Message');
+    }
+    else
+    {
+        jQuery.ajax({
+            url:'send_message.php',
+            type:'post',
+            data:'name='+name+'&email='+email+'&mobile='+mobile+'&message='+message,
+            success:function(result){
+                alert(result);
+            }
+        });
+    }
+    
+
+
+
+}
