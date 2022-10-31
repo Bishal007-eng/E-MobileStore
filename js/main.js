376b93854e85b4ae1e6249eeda7faa02f8738943
Bishal Thapa <bishal.thap3@gmail.com>
2022-10-31 10:32:36 +0545

    Login/Register Validation

--- js/main.js
+++ js/main.js
@@ -432,6 +432,12 @@
 })(jQuery);
 
 
+
+
+
+
+
+
 function send_message()
 {
     var name = jQuery("#name").val();
@@ -470,4 +476,111 @@ function send_message()
 
 
 
+}
+
+
+function user_register(){
+	jQuery('.field_error').html('');
+	var name=jQuery("#name").val();
+	var email=jQuery("#email").val();
+	var mobile=jQuery("#mobile").val();
+	var password=jQuery("#password").val();
+	var is_error='';
+	if(name=="")
+    {
+		jQuery('#name_error').html('Please enter name');
+		is_error='yes';
+	}
+    if(email=="")
+    {
+		jQuery('#email_error').html('Please enter email');
+		is_error='yes';
+	}
+    if(mobile=="")
+    {
+		jQuery('#mobile_error').html('Please enter mobile');
+		is_error='yes';
+	}
+    if(password=="")
+    {
+		jQuery('#password_error').html('Please enter password');
+		is_error='yes';
+	}
+	if(is_error=='')
+    {
+		jQuery.ajax({
+			url:'register_submit.php',
+			type:'post',
+			data:'name='+name+'&email='+email+'&mobile='+mobile+'&password='+password,
+			success:function(result){
+				if(result=='email_present'){
+					jQuery('#email_error').html('Email id already present');
+				}
+				if(result=='insert'){
+					jQuery('.register_msg p').html('Thank you for registeration');
+				}
+			}	
+		});
+	}
+	
+}
+
+
+function user_login(){
+
+	jQuery('.field_error').html('');
+	var email=jQuery("#login_email").val();
+	var password=jQuery("#login_password").val();
+    
+	var is_error='';
+	if(email=="")
+    {
+		jQuery('#login_email_error').html('Please enter email');
+		is_error='yes';
+	}
+    if(password=="")
+    {
+		jQuery('#login_password_error').html('Please enter password');
+		is_error='yes';
+	}
+	if(is_error=='')
+    {
+		jQuery.ajax({
+			url:'login_submit.php',
+			type:'post',
+			data:'email='+email+'&password='+password,
+			success:function(result){
+				if(result=='wrong'){
+					jQuery('.login_msg p').html('Please enter valid login details');
+				}
+				if(result=='valid'){
+					window.location.href='index.php';
+				}
+			}	
+		});
+	}	
+}
+
+
+function manage_cart(pid,type){
+
+	if(type=='update')
+    {
+		var qty=jQuery("#"+pid+"qty").val();
+	}
+    else
+    {
+		var qty=jQuery("#qty").val();
+	}
+	jQuery.ajax({
+		url:'manage_cart.php',
+		type:'post',
+		data:'pid='+pid+'&qty='+qty+'&type='+type,
+		success:function(result){
+			if(type=='update' || type=='remove'){
+				window.location.href='cart.php';
+			}
+			jQuery('.htc__qua').html(result);
+		}	
+	});	
 }
--- login.php
+++ login.php
@@ -1,8 +1,13 @@
 <?php 
 
+
 	require('top.php');
 
+
+
 ?>
+
+
 
 <div class="ht__bradcaump__area" style="background: rgba(0, 0, 0, 0) url(images/bg/4.jpg) no-repeat scroll center center / cover ;">
   <div class="ht__bradcaump__wrap">
@@ -13,51 +18,62 @@
             <nav class="bradcaump-inner">
               <a class="breadcrumb-item" href="index.php">Home</a>
               <span class="brd-separetor"><i class="zmdi zmdi-chevron-right"></i></span>
-              <span class="breadcrumb-item active">Login</span>
+              <span class="breadcrumb-item active">Login/Register</span>
             </nav>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
+        	</div>
+      	</div>
+    	</div>
+  	</div>
+	</div>
 </div>
 
+        
+<!-- Start Contact Area -->
 <section class="htc__contact__area ptb--100 bg__white">
+
   <div class="container">
 
     <div class="row">
+
 			<div class="col-md-6">
+
 				<div class="contact-form-wrap mt--60">
+
 					<div class="col-xs-12">
 						<div class="contact-title">
 							<h2 class="title__line--6">Login</h2>
 						</div>
 					</div>
 
+
 					<div class="col-xs-12">
-						<form id="contact-form" action="#" method="post">
+						<form id="login-form" method="post">
+
 							<div class="single-contact-form">
 								<div class="contact-box name">
-									<input type="text" name="name" placeholder="Your Email*" style="width:100%">
+									<input type="text" name="login_email" id="login_email" placeholder="Your Email*" style="width:100%">
 								</div>
+								<span class="field_error" id="login_email_error"></span>
 							</div>
-							
+
 							<div class="single-contact-form">
 								<div class="contact-box name">
-									<input type="text" name="name" placeholder="Your Password*" style="width:100%">
+									<input type="password" name="login_password" id="login_password" placeholder="Your Password*" style="width:100%">
 								</div>
+								<span class="field_error" id="login_password_error"></span>
 							</div>
 									
 							<div class="contact-btn">
-								<button type="submit" class="fv-btn">Login</button>
+								<button type="button" class="fv-btn" onclick="user_login()">Login</button>
 							</div>
-
 						</form>
-						<div class="form-output">
-							<p class="form-messege"></p>
-						</div>
 
+						<div class="form-output login_msg">
+							<p class="form-messege field_error"></p>
+						</div>
 					</div>
+
+
 				</div> 
                 
 			</div>
@@ -65,6 +81,7 @@
 
 			<div class="col-md-6">
 				<div class="contact-form-wrap mt--60">
+
 					<div class="col-xs-12">
 						<div class="contact-title">
 							<h2 class="title__line--6">Register</h2>
@@ -72,47 +89,57 @@
 					</div>
 
 					<div class="col-xs-12">
-						<form id="contact-form" action="#" method="post">
+
+						<form id="register-form" method="post">
+
 							<div class="single-contact-form">
 								<div class="contact-box name">
-									<input type="text" name="name" placeholder="Your Name*" style="width:100%">
+									<input type="text" name="name" id="name" placeholder="Your Name*" style="width:100%">
 								</div>
+								<span class="field_error" id="name_error"></span>
 							</div>
 
 							<div class="single-contact-form">
 								<div class="contact-box name">
-									<input type="text" name="name" placeholder="Your Email*" style="width:100%">
+									<input type="text" name="email" id="email" placeholder="Your Email*" style="width:100%">
 								</div>
+								<span class="field_error" id="email_error"></span>
 							</div>
 
 							<div class="single-contact-form">
 								<div class="contact-box name">
-									<input type="text" name="name" placeholder="Your Mobile*" style="width:100%">
+									<input type="text" name="mobile" id="mobile" placeholder="Your Mobile*" style="width:100%">
 								</div>
+								<span class="field_error" id="mobile_error"></span>
 							</div>
 
 							<div class="single-contact-form">
 								<div class="contact-box name">
-									<input type="text" name="name" placeholder="Your Password*" style="width:100%">
+									<input type="password" name="password" id="password" placeholder="Your Password*" style="width:100%">
 								</div>
+								<span class="field_error" id="password_error"></span>
 							</div>
 									
 							<div class="contact-btn">
-								<button type="submit" class="fv-btn">Register</button>
+								<button type="button" class="fv-btn" onclick="user_register()">Register</button>
 							</div>
 						</form>
 
-						<div class="form-output">
-							<p class="form-messege"></p>
+						<div class="form-output register_msg">
+							<p class="form-messege field_error"></p>
 						</div>
 					</div>
 
 				</div> 
-                
+
 			</div>
-		</div>	
+                
+		</div>
+					
   </div>
+	
 </section>
+
 
 
 <?php require('footer.php')?>        
