45234eca0c8c1791dfabecda604da7c74d8a8c5b
Bishal Thapa <bishal.thap3@gmail.com>
2022-10-31 11:23:42 +0545

    Frontend

--- top.php
+++ top.php
@@ -81,7 +81,17 @@
               <div class="col-md-3 col-lg-2 col-sm-4 col-xs-4">
                 <div class="header__right">
                   <div class="header__account">
-                    <a href="login.php">Login/Register</a>
+                    <?php 
+                      if(isset($_SESSION['USER_LOGIN']))
+                      {
+                        echo '<a href="logout.php">Logout</a>';
+                        
+                      }
+                      else
+                      {                       
+                        echo '<a href="login.php">Login/Register</a>';
+                      }
+                    ?>
                   </div>
                   <div class="htc__shopping__cart">
                     <a class="cart__menu" href="#"><i class="icon-handbag icons"></i></a>
