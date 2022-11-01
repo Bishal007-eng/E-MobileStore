eeea2e70913fcda7cd30a77d8c58d591a2453554
Bishal Thapa <bishal.thap3@gmail.com>
2022-11-01 20:45:04 +0545

    Admin panal

--- Admin/top.inc.php
+++ Admin/top.inc.php
@@ -45,7 +45,7 @@ if(isset($_SESSION['ADMIN_LOGIN']) && $_SESSION['ADMIN_LOGIN']!=''){
               <a href="product.php" > Product Master</a>
             </li>
 				    <li class="menu-item-has-children dropdown">
-              <a href="#" > Order Master</a>
+              <a href="order_master.php" > Order Master</a>
             </li>
             <li class="menu-item-has-children dropdown">
               <a href="users.php" > User Master</a>
