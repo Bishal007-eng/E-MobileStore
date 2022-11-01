29f51d1b9ba8b9733fb79f482007294fa37a623a
Bishal Thapa <bishal.thap3@gmail.com>
2022-11-01 12:18:16 +0545

    Successful  Purchase message

---
+++ thank_you.php
@@ -0,0 +1,50 @@
+<?php 
+
+    require('top.php');
+
+?>
+
+
+<div class="ht__bradcaump__area" style="background: rgba(0, 0, 0, 0) url(images/bg/4.jpg) no-repeat scroll center center / cover ;">
+    <div class="ht__bradcaump__wrap">
+        <div class="container">
+            <div class="row">
+                <div class="col-xs-12">
+                    <div class="bradcaump__inner">
+                        <nav class="bradcaump-inner">
+                            <a class="breadcrumb-item" href="index.html">Home</a>
+                            <span class="brd-separetor"><i class="zmdi zmdi-chevron-right"></i></span>
+                            <span class="breadcrumb-item active">Thank You</span>
+                        </nav>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+<div class="checkout-wrap ptb--100">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="checkout__inner">
+                    <div class="accordion-list">
+                        <div class="accordion">
+                            <h2 style="color:red; border: 2px;">Your order has been placed successfully!</h2>
+                            
+									
+						</div>
+                    </div>
+                </div>
+                <br>
+                <br>
+                <button style="border: rounded ;"><a style="font-size: 16px; color: blue; " href="index.php">Go to Home-page</a></button>
+            </div>
+        </div>
+    </div>
+</div>
+       
+
+
+<?php require('footer.php')?>        
